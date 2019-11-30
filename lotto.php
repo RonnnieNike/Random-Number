07244448a4a3a79e3e786673729759cc28b9e3d2
RonnnieNike <52289785+RonnnieNike@users.noreply.github.com>
2019-11-30 12:49:48 +0100

    Add files via upload

---
+++ lotto.php
@@ -0,0 +1,22 @@
+
+<?php
+//estrazione 8 numeri random
+$num1 =rand(1,90);
+$num2 =rand(1,90);
+$num3=rand(1,90);
+$num4 =rand(1,90);
+$num5=rand(1,90);
+$num6=rand(1,90);
+$num7=rand(1,90);
+$num8=rand(1,90);
+
+echo "primo numero estratto ".$num1."<br>" ;
+echo "secondo numero estratto ".$num2."<br>" ;
+echo "terzo numero estratto ".$num3."<br>" ;
+echo "quarto numero estratto ".$num4."<br>" ;
+echo "quinto numero estratto ".$num5."<br>" ;
+echo "sestonumero estratto ".$num6."<br>" ;
+echo "jolly estratto ".$num7."<br>" ;
+echo "superstar estratto ".$num8."<br>" ;
+
+ ?>
